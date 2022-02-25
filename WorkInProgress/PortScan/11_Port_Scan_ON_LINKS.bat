b1570836f813a221b821b2a99b89d8a8ebfdef25
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2022-02-25 10:54:57 -0300

    Create 11_Port_Scan_ON_LINKS.bat

---
+++ WorkInProgress/PortScan/11_Port_Scan_ON_LINKS.bat
@@ -0,0 +1,16 @@
+cls
+
+start http://en.dnstools.ch/port-scan.html
+start http://www.ipv6scanner.com/cgi-bin/main.py
+start http://www.t1shopper.com/tools/port-scan/
+start http://www.webportscanner.com/
+start https://gf.dev/port-scanner
+start https://gf.dev/toolbox
+start https://hackertarget.com/nmap-online-port-scanner/
+start https://nmap.online/
+start https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap
+start https://pentest-tools.com/network-vulnerability-scanning/udp-port-scanner-online-nmap
+start https://spyse.com/tools/port-scanner
+start https://www.nmmapper.com/st/networkmapper/nmap/online-port-scanning/
+start https://www.whatismyip.com/port-scanner/
+start https://www.yougetsignal.com/tools/open-ports/
